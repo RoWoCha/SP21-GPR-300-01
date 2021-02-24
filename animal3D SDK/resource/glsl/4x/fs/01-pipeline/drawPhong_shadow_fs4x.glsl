f5a65ba868110545c96ebaf75dc68f7683bccc65
Egor Fesenko <fesenkoegor239@gmail.com>
2021-02-25 00:09:01 +0300

    Adding passTangentBasis + PhongShadow shader, encoding shaders

--- animal3D SDK/resource/glsl/4x/fs/01-pipeline/drawPhong_shadow_fs4x.glsl
+++ animal3D SDK/resource/glsl/4x/fs/01-pipeline/drawPhong_shadow_fs4x.glsl
@@ -33,12 +33,77 @@
 //	-> perform manual "perspective divide" on shadow coordinate
 //	-> perform "shadow test" (explained in class)
 
+// Info sources: Blue Book ("Casting Shadows" pp.648-654)
+// Phong shader taken from https://github.com/RoWoCha/SP21-GPR-300-01/blob/project1_egor/animal3D%20SDK/resource/glsl/4x/fs/00-common/drawPhong_fs4x.glsl
+
 layout (location = 0) out vec4 rtFragColor;
+layout (binding = 0) uniform sampler2D uTex_shadow;
+
+in vec4 vPosition;
+in vec4 vNormal;
+in vec2 vTexcoord;
+in vec4 vShadowCoord;
 
 uniform int uCount;
 
+uniform vec4 uColor;
+uniform sampler2D uTex_dm;
+uniform sampler2D uTex_sm;
+
+// simple point light
+struct sPointLightData
+{
+	vec4 position;					// position in rendering target space
+	vec4 worldPos;					// original position in world space
+	vec4 color;						// RGB color with padding
+	float radius;					// radius (distance of effect from center)
+	float radiusSq;					// radius squared (if needed)
+	float radiusInv;				// radius inverse (attenuation factor)
+	float radiusInvSq;				// radius inverse squared (attenuation factor)
+};
+
+uniform ubLight
+{
+	sPointLightData uPointLightData[4];
+};
+
+vec4 phongShadingCalc(int lightNum);
+
 void main()
 {
-	// DUMMY OUTPUT: all fragments are OPAQUE MAGENTA
-	rtFragColor = vec4(1.0, 0.0, 1.0, 1.0);
+	vec4 color;
+
+	//Iterating through light sources
+	for(int i = 0; i < uCount; i++)
+	{
+		color += vec4(vec3(phongShadingCalc(i)), 0.0);
+	}
+
+	rtFragColor = textureProj(uTex_shadow, vShadowCoord) * color;	
+}
+
+//Function for calculation of Phong shading from one light source
+vec4 phongShadingCalc(int lightNum)
+{
+	float distance = length(uPointLightData[lightNum].position - vPosition);
+	vec4 lightVec = normalize(uPointLightData[lightNum].position - vPosition);
+	vec4 normal = normalize(vNormal);
+
+	vec4 viewVec = normalize(-vPosition);
+	vec4 reflectionVec = reflect(-lightVec, normal);	
+
+	//diffuse (Lambert) coefficient:
+	float diffCoeff = max(0.0, dot(normal, lightVec));
+	vec4 diffuse_color = diffCoeff * texture(uTex_dm, vTexcoord);
+
+	//specular (Phong) coefficient
+	float specCoeff = max(0.0, dot(viewVec, reflectionVec));
+	specCoeff *= specCoeff;
+	vec4 specular_color = specCoeff * texture(uTex_sm, vTexcoord);
+
+	//adding attenuation
+	vec4 resCoeff = (diffuse_color + specular_color) * (2.0 / (uPointLightData[lightNum].radiusInv * distance * distance + 1.0));
+	vec4 result = resCoeff * uPointLightData[lightNum].color * uColor;
+
+	return result;
 }
--- animal3D SDK/resource/glsl/4x/vs/01-pipeline/passTangentBasis_shadowCoord_transform_vs4x.glsl
+++ animal3D SDK/resource/glsl/4x/vs/01-pipeline/passTangentBasis_shadowCoord_transform_vs4x.glsl
@@ -26,14 +26,14 @@
 
 // ****TO-DO:
 // 1) core transformation and lighting setup:
-//	-> declare data structures for projector and model matrix stacks
+//	-> declare data structures for projector and model matrix stacks +
 //		(hint: copy and slightly modify demo object descriptors)
-//	-> declare uniform block for matrix data
+//	-> declare uniform block for matrix data +
 //		(hint: must match how it is uploaded in update function)
 //	-> use matrix data for current object to perform relevant transformations
 //		(hint: model-view-projection sequence may be split up like last time, 
 //		but per usual the final clip-space result is assigned to gl_Position)
-//	-> declare relevant attributes for lighting
+//	-> declare relevant attributes for lighting +
 //	-> perform any additional transformations and write varyings for lighting
 // 2) shadow mapping
 //	-> using the above setup, perform additional transformation to generate a 
@@ -43,17 +43,72 @@
 //	-> declare and write varying for shadow coordinate
 
 layout (location = 0) in vec4 aPosition;
+layout (location = 2) in vec3 aNormal;
+layout (location = 8) in vec2 aTexcoord;
 
 flat out int vVertexID;
 flat out int vInstanceID;
 
 uniform int uIndex;
 
+// ubo transform:
+// -> projector stack for camera, light
+// -> model stack for all objects
+
+// matrix stack for a viewer object
+struct sProjectorMatrixStack
+{
+	mat4 projectionMat;					// projection matrix (viewer -> clip)
+	mat4 projectionMatInverse;			// projection inverse matrix (clip -> viewer)
+	mat4 projectionBiasMat;				// projection-bias matrix (viewer -> biased clip)
+	mat4 projectionBiasMatInverse;		// projection-bias inverse matrix (biased clip -> viewer)
+	mat4 viewProjectionMat;				// view-projection matrix (world -> clip)
+	mat4 viewProjectionMatInverse;		// view-projection inverse matrix (clip -> world)
+	mat4 viewProjectionBiasMat;			// view projection-bias matrix (world -> biased clip)
+	mat4 viewProjectionBiasMatInverse;	// view-projection-bias inverse matrix (biased clip -> world)
+};
+
+// matrix stack for a single scene object/model
+struct sModelMatrixStack
+{
+	mat4 modelMat;						// model matrix (object -> world)
+	mat4 modelMatInverse;				// model inverse matrix (world -> object)
+	mat4 modelMatInverseTranspose;		// model inverse-transpose matrix (object -> world skewed)
+	mat4 modelViewMat;					// model-view matrix (object -> viewer)
+	mat4 modelViewMatInverse;			// model-view inverse matrix (viewer -> object)
+	mat4 modelViewMatInverseTranspose;	// model-view inverse transpose matrix (object -> viewer skewed)
+	mat4 modelViewProjectionMat;		// model-view-projection matrix (object -> clip)
+	mat4 atlasMat;						// atlas matrix (texture -> cell)
+};
+
+uniform ubTransformStack
+{
+	//sProjectorMatrixStack uProjectorMatrixStack[2];
+	sProjectorMatrixStack uCameraMatrixStack, uLightMatrixStack;
+	sModelMatrixStack uModelMatrixStack[16];
+};
+
+out vec4 vPosition;
+out vec4 vNormal;
+out vec2 vTexcoord;
+out vec4 vShadowCoord;
+
 void main()
 {
 	// DUMMY OUTPUT: directly assign input position to output position
-	gl_Position = aPosition;
+	//gl_Position = aPosition;
+	gl_Position = uCameraMatrixStack.projectionMat *
+			uModelMatrixStack[uIndex].modelViewMat * aPosition;
 
+	vPosition = uModelMatrixStack[uIndex].modelViewMat * aPosition;
+	vNormal = normalize(uModelMatrixStack[uIndex].modelViewMat * vec4(aNormal, 1.0));
+
+	vTexcoord = aTexcoord;
+
+	//vmath::mat4 shadow_matrix = scale_bias_matrix * light_proj_matrix * light_view_matrix * model_matrix;
+	mat4 shadowMatrix = uLightMatrixStack.viewProjectionBiasMat * uModelMatrixStack[uIndex].modelMat;
+	vShadowCoord = shadowMatrix * aPosition;
+	
 	vVertexID = gl_VertexID;
 	vInstanceID = gl_InstanceID;
 }
